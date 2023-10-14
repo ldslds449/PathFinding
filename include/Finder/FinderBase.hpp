0443ce1007ab26bab26ee12b5ee4213a9453f72f
ldslds449 <ldslds449@gmail.com>
2023-10-14 15:50:45 +0800

    Change the log format

--- include/Finder/FinderBase.hpp
+++ include/Finder/FinderBase.hpp
@@ -172,7 +172,7 @@ class FinderBase {
       const TPos &prevPos = pathVec[i - 1], &newPos = pathVec[i],
                  diffPos = newPos - prevPos;
       std::cout << "From: " << prevPos << " To: " << newPos
-                << " Diff: " << diffPos << " (" << i << "/" << path->size()
+                << " Diff: " << diffPos << " (" << i+1 << "/" << path->size()
                 << ")" << std::endl
                 << std::flush;
       bool r;
