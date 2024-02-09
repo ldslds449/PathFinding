17cf3bc01dcf51ffa93c4457b70d637016991e92
ldslds449 <ldslds449@gmail.com>
2024-02-09 17:35:09 +0800

    Using table heap in BiAstarFinder

--- include/Finder/BiAstarFinder.hpp
+++ include/Finder/BiAstarFinder.hpp
@@ -2,10 +2,10 @@
 #define PATHFINDING_FINDER_BIASTARFINDER_H_
 
 #include <chrono>
-#include <queue>
 
 #include "Evaluate/Evaluate.hpp"
 #include "Finder/FinderBase.hpp"
+#include "Heap/TableHeap.hpp"
 #include "Type.hpp"
 #include "Vec3.hpp"
 #include "Weighted/Weighted.hpp"
@@ -27,19 +27,11 @@ class BiAstarFinder
   virtual std::tuple<PathResult, std::shared_ptr<Path<TPos>>, U64> findPathImpl(
       const TPos &from, const goal::GoalBase<TPos> &goal, const U64 &timeLimit,
       const U64 &nodeLimit, const U64 &extraTimeLimit) const override {
-    // a node in A*
-    struct Node {
-      TPos pos;
-      CostT gCost, hCost, fCost;
-      Node(const TPos &p, const CostT &g, const CostT &h)
-          : pos(p), gCost(g), hCost(h), fCost(TWeighted::combine(g, h)) {}
-      Node() = default;
-    };
-
     // record the information of a node
     struct PosInfo {
       TPos parent;
       CostT gCost, hCost;  // the lowest gCost, hCost of key
+      bool closed;         // whether in closed set
     };
 
     // direction to generate next node
@@ -51,20 +43,13 @@ class BiAstarFinder
     // store A star data
     struct DataSet {
      private:
-      // compare function for priority queue, sort Node
-      const std::function<bool(const Node &a, const Node &b)> pq_cmp =
-          [](const Node &a, const Node &b) {
-            // from lowest cost to largest cost
-            return a.fCost > b.fCost;
-          };
-
       const TPos target;
 
      public:
-      std::priority_queue<Node, std::vector<Node>, decltype(pq_cmp)> pq;
+      TableHeap<TPos, CostT, std::less<CostT>> heap;
 
       // key: Position
-      // value: {Parent, gCost of key, hCost of key}
+      // value: {Parent, gCost of key, hCost of key, closed}
       std::unordered_map<TPos, PosInfo> infoTable;
 
       std::function<std::vector<TPos>(const TPos &, const TPos &,
@@ -76,9 +61,10 @@ class BiAstarFinder
       }
 
       DataSet(const TPos &t, decltype(walkable) f)
-          : target(t), pq(pq_cmp), walkable(f) {}
+          : target(t), heap(std::less<CostT>{}), walkable(f) {}
     };
 
+    // create two datasets
     const TPos &to = goal.getGoalPosition();
     DataSet forward(to, [&](const TPos &a, const TPos &b, const float &c) {
       return BASE::isAbleToWalkTo(a, b, c);
@@ -104,34 +90,32 @@ class BiAstarFinder
     const CostT climbCost = TEdgeEval::eval(TPos{0, 1, 0});
 
     // add initial state
-    forward.pq.emplace(from, 0, forward.eval(from));
-    backward.pq.emplace(to, 0, backward.eval(to));
+    forward.heap.update(from, forward.eval(from));
+    backward.heap.update(to, backward.eval(to));
     // gCost and hCost are useless
-    forward.infoTable[from] = {from, 0, 0};
-    backward.infoTable[to] = {to, 0, 0};
+    forward.infoTable[from] = {from, 0, 0, false};
+    backward.infoTable[to] = {to, 0, 0, false};
 
     // for loop to find a path to goal
-    Node last;
+    TPos last;
     bool found = false;
     bool timeUp = false, nodeSearchExceed = false;
     U64 nodeCount = 0;
-    while (!forward.pq.empty() && !backward.pq.empty()) {
-      while (forward.pq.top().gCost >
-             forward.infoTable[forward.pq.top().pos].gCost)
-        forward.pq.pop();
-      while (backward.pq.top().gCost >
-             backward.infoTable[backward.pq.top().pos].gCost)
-        backward.pq.pop();
+    while (forward.heap.size() > 0 && backward.heap.size() > 0) {
+      const CostT f_fcost = forward.heap.lookup(forward.heap.top()),
+                  b_fcost = backward.heap.lookup(backward.heap.top());
+      DataSet &select = (f_fcost <= b_fcost ? forward : backward);
+      auto &otherTable =
+          (f_fcost <= b_fcost ? backward.infoTable : forward.infoTable);
 
-      const Node &fnode = forward.pq.top(), &bnode = backward.pq.top();
-      DataSet &select = (fnode.fCost <= bnode.fCost ? forward : backward);
-      auto &otherTable = (fnode.fCost <= bnode.fCost ? backward.infoTable : forward.infoTable);
-
-      Node now = select.pq.top();
-      select.pq.pop();
+      TPos now = select.heap.extract();
+      // add to close set
+      auto &now_info = select.infoTable[now];
+      now_info.closed = true;
+      CostT nowGcost = now_info.gCost;
 
       // check goal
-      if (otherTable.find(now.pos) != otherTable.end()) {
+      if (otherTable.find(now) != otherTable.end()) {
         // found goal
         found = true;
         last = now;
@@ -154,8 +138,8 @@ class BiAstarFinder
       // find neighbour
       for (const Direction &dir : directions) {
         std::vector<TPos> newOffsets =
-            select.walkable(now.pos, dir.offset, config.fallingDamageTolerance);
-        
+            select.walkable(now, dir.offset, config.fallingDamageTolerance);
+
         for (TPos &newOffset : newOffsets) {
           CostT addGCost = dir.cost;
           if (newOffset.y > 0)
@@ -164,24 +148,25 @@ class BiAstarFinder
             addGCost += fallCost * (-newOffset.y);
 
           // add new position
-          const TPos newPos = now.pos + newOffset;
-          const TPos &parent = now.pos;
-          const CostT newGCost = now.gCost + addGCost;
+          const TPos newPos = now + newOffset;
+          const TPos &parent = now;
+          const CostT newGCost = nowGcost + addGCost;
 
           auto found_it = select.infoTable.find(newPos);
           if (found_it != select.infoTable.end()) {
-            CostT &PreGCost = found_it->second.gCost;
-            // compare the gCost and reserve the one with lower cost
-            if (newGCost < PreGCost) {
-              found_it->second.parent = parent;
-              found_it->second.gCost = newGCost;
-              select.pq.emplace(newPos, newGCost,
-                                found_it->second.hCost);  // lazy deletion
+            if (!found_it->second.closed) {
+              CostT &PreGCost = found_it->second.gCost;
+              // compare the gCost and reserve the one with lower cost
+              if (newGCost < PreGCost) {
+                found_it->second.parent = parent;
+                found_it->second.gCost = newGCost;
+                select.heap.update(newPos, newGCost + found_it->second.hCost);
+              }
             }
           } else {
             CostT newHCost = select.eval(newPos);
-            select.pq.emplace(newPos, newGCost, newHCost);
-            select.infoTable[newPos] = {parent, newGCost, newHCost};
+            select.heap.update(newPos, newGCost + newHCost);
+            select.infoTable[newPos] = {parent, newGCost, newHCost, false};
           }
         }
       }
@@ -192,7 +177,7 @@ class BiAstarFinder
     if (found) {
       // forward
       {
-        TPos nowPos = last.pos;
+        TPos nowPos = last;
         while (true) {
           TPos &newPos = forward.infoTable[nowPos].parent;
           if (newPos == nowPos) break;
@@ -203,7 +188,7 @@ class BiAstarFinder
       }
       // backward
       {
-        TPos nowPos = last.pos;
+        TPos nowPos = last;
         while (true) {
           path->add(nowPos);
           TPos &newPos = backward.infoTable[nowPos].parent;
