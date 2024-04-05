80082d7d3b2caf53d15849203a5d9898fb0fcd76
ldslds449 <ldslds449@gmail.com>
2024-04-05 16:40:02 +0800

    ✨ Refine path

--- include/pf/Path.hpp
+++ include/pf/Path.hpp
@@ -30,6 +30,30 @@ class Path {
   inline void pop_back() { nodes.pop_back(); }
   inline const std::vector<TPos> &get() const { return nodes; }
 
+  // only leave important point
+  inline void refine(const std::size_t &max_interval) {
+    std::vector<TPos> new_nodes;
+    std::size_t interval = 0;
+    for (std::size_t i = 0; i < nodes.size(); ++i) {
+      if (i <= 1) {
+        new_nodes.emplace_back(nodes[i]);
+      } else {
+        TPos prev_vec = nodes[i - 1] - nodes[i - 2];
+        TPos now_vec = nodes[i] - nodes[i - 1];
+        if (prev_vec == now_vec && interval + 1 < max_interval) {
+          // ignore this point
+          interval++;
+        } else {
+          // important point
+          new_nodes.emplace_back(nodes[i]);
+          interval = 0;
+        }
+      }
+    }
+    // exchange vector
+    nodes.swap(new_nodes);
+  }
+
   TPos &operator[](const std::size_t &idx) { return nodes[idx]; }
 };
 
